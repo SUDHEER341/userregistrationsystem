dd4bde48402cf35eb3125c7a501e64eeffda6842
SUDHEER341 <sudheer601220@gmail.com>
2023-03-12 22:29:32 +0530

    Added : validation of Mobile number of the user

--- UserRegistrationValidation.java
+++ UserRegistrationValidation.java
@@ -1,46 +1,74 @@
 package com.userregistrationsystem;
-
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class UserRegistrationValidation {
-
-    public void firstName()
+    public void getName()
     {
         UserRegistrationValidation user=new UserRegistrationValidation();
+        user userDetails=new user();
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter the first name");
-        user userDetails=new user(scanner.nextLine());
-        Pattern pattern=Pattern.compile("([A-Z]{1}[a-z]{2,})");
-        Matcher matcher=pattern.matcher(userDetails.firstName);
-        boolean found=matcher.matches();
-        if(!found) {
-            System.out.println("Please enter first letter in capital and minimum 3 characters");
-            user.firstName();
-        }
-    }
+        System.out.println("please enter the first name");
+        userDetails.setFirstName(scanner.nextLine());
 
-    public void lastName()
-    {
-        UserRegistrationValidation user=new UserRegistrationValidation();
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("PleaseEnter the Last name");
-        user userDetails=new user(scanner.nextLine());
-        Pattern pattern=Pattern.compile("([A-Z]{1}[a-z]{2,})");
-        Matcher matcher=pattern.matcher(userDetails.firstName);
-        boolean found=matcher.matches();
-        if(!found)
+        System.out.println(" please enter the last name");
+        userDetails.setLastName(scanner.nextLine());
+
+        Pattern p=Pattern.compile("([A-Z][a-z]{2,})");
+        Matcher matcherFirstName=p.matcher(userDetails.getFirstName());
+        boolean matchFirstName=matcherFirstName.matches();
+        Matcher matcherLastName=p.matcher(userDetails.getLastName());
+        boolean matchLastName=matcherLastName.matches();
+
+        if(!matchFirstName || !matchLastName)
         {
-            System.out.println("Please enter first letter of last name caps and minimum 3 characters");
-            user.lastName();
+            System.out.println("Please enter first letter in capital " +
+                                "and minimum 3 characters");
+            user.getName();
         }
     }
+    public void getEmailId()
+    {
+        UserRegistrationValidation user=new UserRegistrationValidation();
+        user userDetails=new user();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the Password");
+        userDetails.setEmailAddress(scanner.nextLine());
+
+        Pattern p=Pattern.compile("([a-zA-Z0-9\\.]+@[a-z]+\\.[a-z]{2,3}+([\\.][a-z]{2})?)");
+        Matcher matcher=p.matcher(userDetails.getEmailAddress());
+        boolean matchPassword=matcher.matches();
+
+        if(!matchPassword)
+        {
+            System.out.println("Please enter correct password");
+            user.getEmailId();
+        }
+    }
+    public void getMobileNumber()
+    {
+        UserRegistrationValidation user=new UserRegistrationValidation();
+        user userDetails=new user();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please enter the Mobile number ");
+        userDetails.setMobileNo(scanner.nextLine());
+
+        Pattern pattern=Pattern.compile("([0-9]{2}+\\s[0-9]{10})");
+        Matcher matcher=pattern.matcher(userDetails.getMobileNo());
+        boolean matchPassword=matcher.matches();
+
+        if(!matchPassword)
+        {
+            System.out.println("Please enter correct mobile no");
+            user.getMobileNumber();
+        }
+    }
+
     public static void main(String[] args)
     {
-        UserRegistrationValidation user=new UserRegistrationValidation();
-        user.firstName();
-        user.lastName();
+        UserRegistrationValidation user = new UserRegistrationValidation();
+        user.getName();
+        user.getEmailId();
+        user.getMobileNumber();
     }
 }
--- user.java
+++ user.java
@@ -1,9 +1,51 @@
 package com.userregistrationsystem;
 public class user {
-    String firstName;
+    private String firstName;
+    private String  lastName;
+    private String  emailAddress;
+    private String mobileNo;
 
-    public user(String firstName){
-        this.firstName=firstName;
+    public user() {
+
     }
-    public user(){}
+    public String getMobileNo() {
+        return mobileNo;
+    }
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+    public user(String firstName, String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }public user(String emailAddress){
+        this.emailAddress=emailAddress;
+    }
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress()
+    {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress)
+    {
+        this.emailAddress = emailAddress;
+    }
 }
